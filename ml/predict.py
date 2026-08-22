22c67dbc384d230f21c1d4534cb4005e26a5db7f
Pawan Kumar <kp355576@@gmail.com>
2026-08-22 12:48:21 +0530

    Complete Phase 1 tasks

--- ml/predict.py
+++ ml/predict.py
@@ -6,15 +6,12 @@ model_path = "ml/saved_model/model.joblib"
 model = joblib.load(model_path)
 
 # Sample Iris flower data
-
-sample = [[5.1, 3.5, 1.4, 2.0]]
+sample = [[5.1, 3.5, 1.4, 0.2]]
 
 # Make prediction
-
 prediction = model.predict(sample)
 
 # Iris class names
-
 class_names = ["setosa", "versicolor", "virginica"]
 
 predicted_class = class_names[prediction[0]]
